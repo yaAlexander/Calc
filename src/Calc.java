e3cce9a3b9b70bc8d6eb6dd9f1773919b36295b0
yaAlexander <ya.alexander@outlook.com>
2020-07-09 17:04:11 +0400

    Создали метод для сложения двух чисел

---
+++ src/Calc.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
+public class Calc {
+
+
+    public static double Addition(Object one, Object two) {
+        double result = (Double) one + (Double) two;
+        return result;
+    }
+
+}
+
